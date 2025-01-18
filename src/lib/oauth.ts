133aa3162155b3ef6aedf4ec42818317ea1146e5
deshmukh3421 <udaydeshmukh1864@gmail.com>
2025-01-18 21:11:34 +0530

    40- oauth changes commit

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -26,8 +26,8 @@ export async function signUpWithGoogle() {
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${origin}/oauth`,
-		`${origin}/signup`,
+		`${process.env.NEXT_PUBLIC_APP}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP}/signup`,
 	);
 
 	return redirect(redirectUrl);
